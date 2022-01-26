8d3774aafb23a6134ce5d077290b82a77968305d
zgg2001 <397606244@qq.com>
2022-01-26 20:55:10 +0800

    数据结构 -- 组

---
+++ src/server/data/group.cpp
@@ -0,0 +1,136 @@
+/*************************************************************************
+	> File Name: group.cpp
+	> Author: ZHJ
+	> Remarks: 数据结构 -- 组
+	> Created Time: Tue 25 Jan 2022 09:30:37 PM CST
+ ************************************************************************/
+
+#include"group.h"
+
+group::group(int g_id, int a_id):
+    group_id(g_id),
+    plan_max_id(0),
+    admin_id(a_id)
+{
+
+}
+
+int 
+group::addplan(int u_id, int status,
+               int plan_id, int user_id,
+               int create_t, int plan_t,
+               const char* content_str, const char* remark_str)
+{
+    //计算新结点的组内id
+    int p_id = plan_max_id + 1;
+    return _add_plan(p_id, u_id, status,
+                     plan_id, user_id,
+                     create_t, plan_t,
+                     content_str, remark_str);
+}
+
+int 
+group::addplan(int p_id, int u_id, int status,
+               int plan_id, int user_id,
+               int create_t, int plan_t,
+               const char* content_str, const char* remark_str)
+{
+    return _add_plan(p_id, u_id, status,
+                     plan_id, user_id,
+                     create_t, plan_t,
+                     content_str, remark_str);
+}
+
+int 
+group::_add_plan(int p_id, int u_id, int status,
+                 int plan_id, int user_id,
+                 int create_t, int plan_t,
+                 const char* content_str, const char* remark_str)
+{
+    //用户不存在
+    if(users.count(u_id) == 0)
+        return -1;
+    //计划编号重复
+    if(plans.count(p_id) != 0)
+        return -2;
+    //添加计划节点
+    plans.insert({ p_id, 
+                 { plan_id, user_id, 
+                   create_t, plan_t,
+                   content_str, remark_str } });
+    plans[p_id].status = static_cast<plan_status>(status);
+    //将此计划节点登记至所属用户下
+    users[u_id].addplan(status, p_id);
+    //更新最大组内id
+    plan_max_id = std::max(plan_max_id, p_id);
+    return p_id;
+}
+
+int
+group::delplan(int u_id, int p_id)
+{
+    if(users.count(u_id) == 0 || plans.count(p_id) == 0)
+        return -1;
+    if(users[u_id].delplan(p_id) == false && u_id != 1 && u_id != admin_id)
+        return -2;
+    return plans.erase(p_id) ? 1 : 0;
+}
+
+int 
+group::updplan_t(int u_id, int p_id, int time)
+{
+    if(users.count(u_id) == 0 || plans.count(p_id) == 0)
+        return -1;
+    if(users[u_id].search(p_id) == false && u_id != 1 && u_id != admin_id)
+        return -2;
+    //计划
+    plans[p_id].plan_time = time;
+    return 1;
+}
+
+int 
+group::updplan_s(int u_id, int p_id, int status)
+{
+    if(users.count(u_id) == 0 || plans.count(p_id) == 0)
+        return -1;
+    if(users[u_id].search(p_id) == false && u_id != 1 && u_id != admin_id)
+        return -2;
+    //用户
+    if(users[u_id].updplan(p_id, status) == false)
+        return -3;
+    //计划
+    plans[p_id].status = static_cast<plan_status>(status);
+    return 1;
+}
+
+int 
+group::updplan_c(int u_id, int p_id,
+                 const char* content_str, const char* remark_str)
+{
+    if(users.count(u_id) == 0 || plans.count(p_id) == 0)
+        return -1;
+    if(users[u_id].search(p_id) == false && u_id != 1 && u_id != admin_id)
+        return -2;
+    //计划
+    plans[p_id].change(content_str, remark_str);
+    return 1;
+}
+
+bool 
+group::adduser(int u_id, std::string name, std::string email)
+{
+    if(users.count(u_id))
+        return false;
+    //int, user
+    users.insert({ u_id, { u_id, name, email } });
+    return true;
+}
+
+bool 
+group::deluser(int u_id)
+{
+    return users.erase(u_id);
+}
+
+
+
---
+++ src/server/data/group.h
@@ -0,0 +1,120 @@
+/*************************************************************************
+	> File Name: group.h
+	> Author: ZHJ
+	> Remarks: 数据结构 -- 组
+	> Created Time: Tue 25 Jan 2022 09:30:25 PM CST
+ ************************************************************************/
+
+#ifndef _GROUP_H_
+#define _GROUP_H_
+
+#include<map>
+#include"user.h"
+
+/*
+* 数据结构 -- 组
+* 每一个实例就是一个组
+* 组中储存了所属于此组的计划和用户
+*/
+class group
+{
+public:
+    group(int g_id, int a_id);
+    group(const group&) = delete;
+    group& operator= (const group&) = delete;
+
+    /*
+    * 计划 增加
+    * 第一种实现为无 组内id 的 新节点 添加
+    * 第二种实现为有 组内id 的 老节点 添加
+    * 二者最终都是通过调用 _add_plan() 实现
+    *
+    *    pid: 计划组内id
+    *    uid: 计划所属id
+    * status: 状态
+    * 后6个传参为class plan构造传参
+    * 
+    * 返回值:
+    *  -1 : 所属用户不存在
+    *  -2 : 计划组内id重复
+    * 其余: 计划的组内id
+    */
+    int addplan(int u_id, int status,
+                int plan_id, int user_id,
+                int create_t, int plan_t,
+                const char* content_str, const char* remark_str);
+    int addplan(int p_id, int u_id, int status,
+                int plan_id, int user_id,
+                int create_t, int plan_t,
+                const char* content_str, const char* remark_str);
+
+    /*
+    * 计划 删除
+    * u_id: 执行删除的uid
+    * p_id: 计划的组内id
+    *
+    * 返回值:
+    *  -1 : 用户不存在/计划不存在
+    *  -2 : 无权限删除
+    *   0 : 未知错误
+    *   1 : 成功
+    */
+    int delplan(int u_id, int p_id);
+
+    /*
+    * 计划 改动
+    * u_id: 执行用户id
+    * p_id: 计划的组内id
+    *
+    * 返回值:
+    *  -1 : 用户/计划不存在
+    *  -2 : 无权限删除
+    *  -3 : 状态数值超限
+    *   1 : 成功
+    */
+    int updplan_t(int u_id, int p_id, int time);//完成时间
+    int updplan_s(int u_id, int p_id, int status);//状态
+    int updplan_c(int u_id, int p_id, 
+                  const char* content_str, const char* remark_str);//内容
+
+    /*
+    * 用户 增加
+    *  u_id: 用户id
+    *  name: 用户名
+    * email: 用户邮箱
+    * 返回true为添加成功
+    */
+    bool adduser(int u_id, std::string name, std::string email);
+
+    /*
+    * 用户 删除
+    * u_id: 用户id
+    * 返回true为删除成功
+    */
+    bool deluser(int u_id);
+
+private:
+    int _add_plan(int p_id, int u_id, int status,
+                  int plan_id, int user_id,
+                  int create_t, int plan_t,
+                  const char* content_str, const char* remark_str);
+
+public:
+    /*
+    *    group_id: 此组的id
+    * plan_max_id: 当前组内计划最大id号 用于分配组内id
+    *    admin_id: 管理员id
+    */
+    int group_id;
+    int plan_max_id;
+    int admin_id;
+
+    /*
+    * plans: 储存了此组中的计划 key为组内id
+    * users: 储存了此组中的用户 key为用户id
+    */
+    std::unordered_map<int, plan> plans;
+    std::unordered_map<int, user> users;
+};
+
+#endif
