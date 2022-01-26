4efaf5f38a0282e41d90d57146fb9f21f44c556d
zgg2001 <397606244@qq.com>
2022-01-26 13:11:37 +0800

    数据结构 -- 用户

---
+++ src/server/data/user.cpp
@@ -0,0 +1,90 @@
+/*************************************************************************
+	> File Name: user.cpp
+	> Author: ZHJ
+	> Remarks: 数据结构 -- 用户 
+	> Created Time: Tue 25 Jan 2022 09:31:03 PM CST
+ ************************************************************************/
+
+#include"user.h"
+
+user::user(int u_id, std::string name, std::string email):
+    user_id(u_id),
+    username(name),
+    mail(email)
+{
+    plans.assign(3, std::list<int>{});
+}
+
+void 
+user::addplan(plan_status status, int id)
+{
+    int sta = static_cast<int>(status);
+    if(sta < 0 || sta > 2)
+        return;
+    plans[sta].push_back(id);
+}
+
+void 
+user::addplan(int status, int id)
+{
+    if(status < 0 || status > 2)
+        return;
+    plans[status].push_back(id);
+}
+
+bool 
+user::delplan(int id)
+{
+    for(int i : {1, 0, 2})//进行中/搁置/已完成
+    {
+        auto iter = std::find(plans[i].begin(), plans[i].end(), id);
+        if(iter != plans[i].end())
+        {
+            //删除
+            plans[i].erase(iter);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool 
+user::updplan(int id, plan_status newsta)
+{
+    int sta = static_cast<int>(newsta);
+    if(sta < 0 || sta > 2)
+        return false;
+    return _update_plan(id, sta); 
+}
+
+bool 
+user::updplan(int id, int newsta)
+{
+    if(newsta < 0 || newsta > 2)
+        return false;
+    return _update_plan(id, newsta); 
+}
+
+bool 
+user::_update_plan(int id, int newsta)
+{
+    for(int i : {1, 0, 2})//进行中/搁置/已完成
+    {
+        auto iter = std::find(plans[i].begin(), plans[i].end(), id);
+        if(iter != plans[i].end())
+        {
+            //状态没有改变直接返回
+            if(newsta == i)
+                return true;
+            //删除并重新加入
+            plans[i].erase(iter);
+            addplan(newsta, id);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
---
+++ src/server/data/user.h
@@ -0,0 +1,81 @@
+/*************************************************************************
+	> File Name: user.h
+	> Author: ZHJ
+	> Remarks: 数据结构 -- 用户
+	> Created Time: Tue 25 Jan 2022 09:30:54 PM CST
+ ************************************************************************/
+
+#ifndef _USER_H_
+#define _USER_H_
+
+#include<string>
+#include<vector>
+#include<list>
+#include<algorithm>
+#include"plan.h"
+
+/*
+* 数据结构 -- 用户
+* 每一个实例就是一个用户
+*
+*  user_id: 用户id
+* username: 用户名
+*     mail: 用户邮箱
+*    plans: 储存此用户计划的组内id
+*           plans[0] = 搁置计划组
+*           plans[1] = 进行中计划组
+*           plans[2] = 已完成计划组
+*/
+class user
+{
+public:
+    user(int u_id, std::string name, std::string email);
+    user(const user&) = delete;
+    user& operator= (const user&) = delete;
+
+    /*
+    * 添加新计划
+    * 依据计划的状态加入对应的链表
+    * 两种实现 按强枚举状态加入 和 按int状态加入
+    * 
+    * status: 计划状态
+    *         0 - 搁置
+    *         1 - 进行中 
+    *         2 - 已完成
+    *     id: 计划的组内id
+    */
+    void addplan(plan_status status, int id);
+    void addplan(int status, int id);
+
+    /*
+    * 删除指定计划
+    *
+    * id: 计划的组内id
+    *
+    * 返回true为删除成功
+    */
+    bool delplan(int id);
+
+    /*
+    * 更新计划状态
+    * 内部调用 _update_plan() 实现
+    *
+    *     id: 要更新的计划id(组内id)
+    * newsta: 新的计划状态 0/1/2
+    *
+    * 返回true为变更成功
+    */
+    bool updplan(int id, plan_status newsta);
+    bool updplan(int id, int newsta);
+
+private:
+    bool _update_plan(int id, int newsta);
+
+public:
+    int user_id;
+    std::string username;
+    std::string mail;
+    std::vector<std::list<int>> plans;
+};
+
+#endif
