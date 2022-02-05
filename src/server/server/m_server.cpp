12b4540bee03c61522af55e0f8e5465e1c3ced39
zgg2001 <397606244@qq.com>
2022-02-05 20:57:25 +0800

    服务端类 -- 组节点与数据库反序列化

--- src/server/server/m_server.cpp
+++ src/server/server/m_server.cpp
@@ -9,10 +9,13 @@
 
 m_server::m_server():
     _sock(INVALID_SOCKET),
-    _lnode(nullptr)
+    _lnodes(nullptr)
 {
+    //数据库mgr
+    _db = new m_db_mgr(this);
+
     //登录节点
-    _lnode = new m_login_node(0, this);
+    _lnodes = new m_login_node(0, this);
 }
 
 m_server::~m_server()
@@ -37,7 +40,7 @@ m_server::m_init()
     } 
 
     //数据库读取
-    //
+    _db->init("localhost", "root", "Zhj@7512");
     
     INFO("Server init(%d) success", _sock);
     return 0;
@@ -64,6 +67,8 @@ m_server::m_bind(const char* ip, unsigned short port)
     if(bind(_sock, (sockaddr*)&_myaddr, sizeof(sockaddr_in)) == SOCKET_ERROR)
     {
         ERROR("Server bind(%hd) faild -- %s", port, strerror(errno));
+        close(_sock);
+        _sock = INVALID_SOCKET;
         return -1;
     }
     else
@@ -87,6 +92,8 @@ m_server::m_listen(int n)
     if(listen(_sock, n) == SOCKET_ERROR) 
     {
         ERROR("Server listen(%d) faild -- %s", n, strerror(errno));
+        close(_sock);
+        _sock = INVALID_SOCKET;
         return -2;
     }
     else
@@ -112,7 +119,7 @@ m_server::m_accept()
     }
     
     //连接后分配给登录节点
-    _lnode->addclient(new m_client_node(temp_socket));
+    _lnodes->addclient(new m_client_node(temp_socket));
     
     INFO("Server accept(%d) success from %s", 
          temp_socket, inet_ntoa(clientAddr.sin_addr));
@@ -122,18 +129,22 @@ m_server::m_accept()
 void
 m_server::m_close()
 {
-    if(_lnode != nullptr)
-        delete _lnode;
+    delete _lnodes;
+    //free组节点
+    for(auto& iter : _gnodes)
+    { 
+        delete iter.second;
+    }
     close(_sock);
 }
 
 void
 m_server::m_start()
 {
-    if(_lnode != nullptr)
+    if(_lnodes != nullptr)
     {
         INFO("Server login_node start");
-        _lnode->start();
+        _lnodes->start();
     }
     else
     {
@@ -146,6 +157,13 @@ m_server::m_work()
 {
     INFO("Server work start");
     
+    //无效套接字 
+    if(_sock == INVALID_SOCKET)
+    {
+        ERROR("Server work faild -- server socket not initialized");
+        return;
+    }
+
     //epoll
     int epollfd = epoll_create(1);//主线程epoll仅关注连接事件
     struct epoll_event ev;
@@ -181,6 +199,37 @@ m_server::m_work()
     close(epollfd);
     INFO("Server work finish");
 }
+
+void 
+m_server::new_group_node(int g_id, const char* g_name, int a_id)
+{
+    _gnodes[g_id] = new m_group_node(g_id, g_name, a_id, this);
+}
+
+void
+m_server::new_group_user(int g_id, int u_id, const char* name, const char* email)
+{
+    _gnodes[g_id]->adduser(u_id, name, email);
+}
+
+void 
+m_server::new_group_plan(int g_id, int p_id, int u_id, int status,
+                         int plan_id, int user_id,
+                         int create_t, int plan_t,
+                         const char* content_str, const char* remark_str)
+{
+    _gnodes[g_id]->addplan(p_id, u_id, status, plan_id, user_id,
+                           create_t, plan_t, content_str, remark_str);
+}
+
+std::pair<int, int> 
+m_server::login(const char* name, const char* passwd)
+{
+    std::string n {name};
+    std::string p {passwd};
+    return _db->login(n, p);
+}
+
 
 
 
--- src/server/server/m_server.h
+++ src/server/server/m_server.h
@@ -18,7 +18,9 @@
 #include<string.h>
 #include<errno.h>
 
+#include<server/database/m_db_mgr.h>
 #include<server/server_node/m_login_node.h>
+#include<server/server_node/m_group_node.h>
 #include<public/log/log.h>
 
 class m_server
@@ -35,16 +37,13 @@ public:
     m_server& operator= (const m_server&) = delete;
 
     /* 
-    * 初始化
-    * 数据读取/socket环境配置
+    * 环境/数据初始化
     * 返回0为成功
     */
     int m_init();
 
     /*
     * 绑定ip/端口
-    *   ip: 监听ip (nullptr为全部)
-    * port: 监听端口
     * 返回0为成功
     */
     int m_bind(const char* ip, unsigned short port);
@@ -62,27 +61,53 @@ public:
     */
     int m_accept();
 
-    /*
-    * 关闭
-    */
+    // 关闭server
     void m_close();
 
-    /*
-    * 启动登录节点
-    */
+    // 启动登录节点
     void m_start();
 
-    /*
-    * 开始工作
-    */
+    //开始工作
     void m_work();
+
+    /*
+    * (反序列化)
+    * 新增组节点
+    * 新增组用户
+    * 新增组计划
+    *
+    * g_id: 组id 
+    * 其余参数参考m_group内的addplan/adduser方法
+    */
+    void new_group_node(int g_id, const char* g_name, int a_id);
+    void new_group_user(int g_id, int u_id, const char* name, const char* email);
+    void new_group_plan(int g_id, int p_id, int u_id, int status,
+                        int plan_id, int user_id,
+                        int create_t, int plan_t,
+                        const char* content_str, const char* remark_str);
+    
+    /*
+    * 登录比对
+    *
+    * 返回值:
+    * pair<uid, gid>
+    * 登录失败时uid = gid = -1
+    * 否则为用户的uid/gid
+    */
+    std::pair<int, int> login(const char* name, const char* passwd);
 
 private:
     //服务器socket
     SOCKET _sock;
 
+    //数据库管理者
+    m_db_mgr* _db;
+
     //登录节点
-    m_login_node* _lnode;
+    m_login_node* _lnodes;
+
+    //组节点
+    std::unordered_map<SOCKET, m_group_node*> _gnodes;
 };
 
 #endif
